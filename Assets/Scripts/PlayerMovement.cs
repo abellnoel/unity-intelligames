dbb408716d440487251f114d33d8bc99613109e6
nabellan1 <59884349+nabellan1@users.noreply.github.com>
2020-10-15 02:43:23 -0400

    Add basic, constant speed, player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] float moveSpeed = 20f;
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //returns -1 left/up, 0 neutral, +1 right/down
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
